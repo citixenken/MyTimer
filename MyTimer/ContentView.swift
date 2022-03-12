9355a258396a6f32b92d590b8e60ecc615a1903b
citixenken <kmuyesu@gmail.com>
2022-03-12 22:30:14 +0300

    designing the UI

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var timerMode: TimerMode = .initial
+    @State private var timerMode: TimerMode = .paused
+    @State private var selectedPicker = 0
+    let availableMinutes = Array(1...59)
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("60")
+                    .font(.system(size: 80))
+                    .padding(.top, 80)
+                
+                Image(systemName: timerMode == .running ? "play.circle.fill" : "pause.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(.orange)
+                
+                if timerMode == .paused {
+                    Image(systemName: "gobackward")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .padding(.top, 40)
+                    
+                }
+                
+                if timerMode == .initial {
+                    Picker(selection: $selectedPicker, label: Text(""), content: {
+                        ForEach(0..<availableMinutes.count) {
+                            Text("\(availableMinutes[$0]) min")
+                        }
+                    } )
+                        .pickerStyle(.wheel)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
