d66a6d7049ba95be7fb1268946fd372ce7a02ce2
citixenken <kmuyesu@gmail.com>
2022-03-12 21:31:40 +0300

    initial commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Ken Muyesu on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
